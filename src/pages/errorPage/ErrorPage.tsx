829bb54d6940bcd8e547cbd1d04ccaa8f8cf843c
chaewon <juh7814@gmail.com>
2023-09-26 19:08:12 +0900

    style: 에러 페이지 생성

--- src/pages/errorPage/ErrorPage.tsx
+++ src/pages/errorPage/ErrorPage.tsx
@@ -1,7 +1,8 @@
+import './ErrorPage.scss';
 const ErrorPage = () => {
     return (
-        <div>
-            에러페이지
+        <div className='error'>
+            <h2 className='h2'>Page Not Found</h2>
         </div>
     );
 };

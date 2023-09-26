7775d64b8e8ffb90f3b695b5ed4b17a48edbeca6
chaewon <juh7814@gmail.com>
2023-09-26 20:04:40 +0900

    style: header UI 완성

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,26 +1,13 @@
-import { useState } from 'react';
 import './Header.scss';
+import SearchForm from '../searchForm/SearchForm';
+
+
 const Header = () => {
-    const [isVisibleSearchForm, setIsVisibleSearch] = useState(false)
-    const onShowSearchForm = () => {
-        setIsVisibleSearch(current => !current)
-    }
+
     return (
         <header className="header">
             <h1 className="h1">Hello, Pokemon!</h1>
-            <button className="magnify" onClick={onShowSearchForm}>
-                돋보기 아이콘
-            </button>
-            {isVisibleSearchForm &&
-                <form>
-                    <fieldset>
-                        <legend>검색창</legend>
-                        <label>검색폼</label>
-                        <input placeholder="포켓몬 이름을 입력해주세요" />
-                        <button>검색</button>
-                    </fieldset>
-                </form>}
-
+            <SearchForm />
         </header>
     );
 };

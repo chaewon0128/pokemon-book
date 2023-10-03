bb3dbf21775791a1c65eb399a850905689ab6210
chaewon <juh7814@gmail.com>
2023-10-03 21:02:04 +0900

    refactor: onClick 이벤트 추가

--- src/components/pokemonFigure/PokemonFigure.tsx
+++ src/components/pokemonFigure/PokemonFigure.tsx
@@ -1,14 +1,14 @@
 import './PokemonFigure.scss';
 import { addTagToNumber } from "../../utils/addTagToNumber";
-import { IPokeminFigure } from '../../types';
+import { IPokemonFigure } from '../../types';
 
 
-const PokemonFigure = ({ name, src, id, type }: IPokeminFigure) => {
+const PokemonFigure = ({ name, src, id, type, onMoveDetail }: IPokemonFigure) => {
     return (
-        <figure className={`pokemon-info ${type}`}>
+        <figure className={`pokemon-info ${type}`} onClick={() => onMoveDetail && onMoveDetail(id)}>
             <img src={src} alt={`${name}의 이미지`} />
             <figcaption>{name}</figcaption>
-            <span>{addTagToNumber(id)}</span>
+            <span>{addTagToNumber(String(id))}</span>
         </figure>
     );
 };

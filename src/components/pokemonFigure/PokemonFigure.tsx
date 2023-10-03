a3ee409973f6cb0cd7630f53a59bc55c535cfb68
chaewon <juh7814@gmail.com>
2023-10-03 23:48:52 +0900

    docs: 오타 수정

--- src/components/pokemonFigure/PokemonFigure.tsx
+++ src/components/pokemonFigure/PokemonFigure.tsx
@@ -5,7 +5,7 @@ import { IPokemonFigure } from '../../types';
 
 const PokemonFigure = ({ name, src, id, type, onMoveDetail }: IPokemonFigure) => {
     return (
-        <figure className={`pokemon-info ${type}`} onClick={() => onMoveDetail && onMoveDetail(id)}>
+        <figure className={`pokemonInfo ${type}`} onClick={() => onMoveDetail && onMoveDetail(id)}>
             <img src={src} alt={`${name}의 이미지`} />
             <figcaption>{name}</figcaption>
             <span>{addTagToNumber(String(id))}</span>

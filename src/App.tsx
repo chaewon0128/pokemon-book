088f4219e6b182ae9227d8ed26f2b0d3985e04e4
chaewon <juh7814@gmail.com>
2023-10-03 23:54:42 +0900

    feat: 웹 접근성 향상을 위한 sr-only 클래스 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import './App.scss'
 import { Outlet } from 'react-router-dom';
+import Header from './components/header/Header';
+import Footer from './components/footer/Footer';
 
 function App() {
   return (
     <>
+      <Header />
       <Outlet />
+      <Footer />
     </>
   )
 }
---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <footer>
+            @copyright pokeapi.co
+        </footer>
+    );
+};
+
+export default Footer;

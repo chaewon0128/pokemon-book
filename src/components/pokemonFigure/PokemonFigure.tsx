e80a2869bb088a5fa9ffbd4fbd9fd2bdd71d60dc
chaewon <juh7814@gmail.com>
2023-10-06 11:48:42 +0900

    refactor: 이미지  Lazy Loading 속성 추가

--- src/components/pokemonFigure/PokemonFigure.tsx
+++ src/components/pokemonFigure/PokemonFigure.tsx
@@ -5,7 +5,7 @@ import { IPokemonFigure } from '../../types';
 const PokemonFigure = ({ name, src, id, type, onMoveDetail }: IPokemonFigure) => {
     return (
         <figure className={`pokemonInfo ${type}`} onClick={() => onMoveDetail && onMoveDetail(id)}>
-            <img src={src} alt={`${name}의 이미지`} />
+            <img src={src} alt={`${name}의 이미지`} loading='lazy' />
             <figcaption>{name}</figcaption>
             <span>{addTagToNumber(String(id))}</span>
         </figure>

fdf18adbdff9c46cfed99a35e946a7a5c7434199
chaewon <juh7814@gmail.com>
2023-10-06 11:41:25 +0900

    refactor: 사용하지 않는 코드 제거

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,10 +1,5 @@
 import { atom } from "recoil";
 
-export const allPokemonState = atom({
-  key: 'allPokemonState',
-  default: [],
-});
-
 export const searchValueState = atom({
   key: 'searchValueState',
   default: '',

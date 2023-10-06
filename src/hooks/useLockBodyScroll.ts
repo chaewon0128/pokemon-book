9146f21e9cc075f2f60fae770af723786330ca02
chaewon <juh7814@gmail.com>
2023-10-06 13:32:23 +0900

    style: card Position fixed로 수정

--- src/hooks/useLockBodyScroll.ts
+++ src/hooks/useLockBodyScroll.ts
@@ -1,12 +1,6 @@
 import { useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
 
 function useLockBodyScroll() {
-  const { pathname } = useLocation();
-
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [pathname]);
 
   useEffect(() => {
     const scrollbarWidth = window.innerWidth - document.body.clientWidth;
